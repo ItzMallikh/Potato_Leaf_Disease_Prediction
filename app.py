ebe0822efc90cfe9bf2dc6cd0d5bcdb0d092c2bb
Itzmallikh@123 <126792968+ItzMallikh@users.noreply.github.com>
2025-02-07 21:10:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import gdown
 import os
 
 file_id = "1gwbsEcekJQvpD_Ki5OVgx0LDsr8iuwpU"
-url = "https://drive.google.com/file/d/1gwbsEcekJQvpD_Ki5OVgx0LDsr8iuwpU/view?usp=sharing"
+url = "https://drive.google.com/file/d/1gwbsEcekJQvpD_Ki5OVgx0LDsr8iuwpU"
        
 model_path = "trained_potato_plant_disease_model-1.keras"
 
@@ -15,9 +15,9 @@ if not os.path.exists(model_path):
     gdown.download(url,model_path,quiet=False)
 
 
-@st.cache_resource()
+
 def load_model():
-    return tf.keras.models.load_model("trained_potato_plant_disease_model-1.keras")
+    return tf.keras.models.load_model(model_path)
 
 def model_prediction(test_image):
     model=load_model()
